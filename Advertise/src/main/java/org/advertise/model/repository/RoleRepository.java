8795fde430871fe1a732e35408efb55b5767845b
bhagyashree111596 <pbhagayshree94@gmail.com>
2024-01-04 15:15:42 +0530

    project

--- Advertise/src/main/java/org/advertise/model/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package org.advertise.model.repository;
-
-import org.advertise.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long>{
-
-}
